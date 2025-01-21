3b1ae09bd98f34c3ba9e8d120e7e23fc18c6e3e5
Artem Shcherbatiuk <vertex451@gmail.com>
2025-01-21 11:58:08 +0100

    feat: kcp-listener follow up (platform-mesh/kubernetes-graphql-gateway#30)

--- services/kubernetes-graphql-gateway/listener/flags/operator_flags.go
+++ services/kubernetes-graphql-gateway/listener/flags/operator_flags.go
@@ -10,11 +10,12 @@ type Flags struct {
 	ProbeAddr              string `envconfig:"default=:8081,optional"`
 	SecureMetrics          bool   `envconfig:"default=true,optional"`
 	EnableHTTP2            bool   `envconfig:"default=false,optional"`
-	OpenAPIdefinitionsPath string `envconfig:"default=./bin/definitions,optional"`
+	OpenAPIdefinitionsPath string `envconfig:"OPEN_API_DEFINITIONS_PATH,default=./bin/definitions"`
 }
 
 func NewFromEnv() (*Flags, error) {
 	opFlags := &Flags{}
 	err := envconfig.Init(opFlags)
+
 	return opFlags, err
 }
