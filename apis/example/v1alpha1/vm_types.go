695cd54e45844fb87c2557a395a9c27ee60b2b49
Nelo-T. Wallus <n.wallus@sap.com>
2025-09-11 14:28:18 +0200

    Update vms.example.platform-mesh.io
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- apis/example/v1alpha1/vm_types.go
+++ apis/example/v1alpha1/vm_types.go
@@ -4,29 +4,21 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // VMSpec defines the desired state of VM
 type VMSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of VM. Edit vm_types.go to remove/update
+	// The architecture of the VM.
 	// +optional
-	Foo *string `json:"foo,omitempty"`
+	// +kubebuilder:validation:Enum=x86_64;arm64
+	Arch string `json:"arch,omitempty"`
+
+	// The amount of memory (in MiB) allocated to the VM.
+	// +optional
+	// +kubebuilder:validation:Minimum=128
+	Memory int `json:"memory,omitempty"`
 }
 
 // VMStatus defines the observed state of VM.
 type VMStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
 	// conditions represent the current state of the VM resource.
 	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
 	//
