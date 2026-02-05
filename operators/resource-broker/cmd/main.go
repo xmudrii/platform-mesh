47fc3f999908b8726c3916bde8d88a6fb0d6748d
Nelo-T. Wallus <n.wallus@sap.com>
2026-02-05 09:20:03 +0100

    Update resource-broker docstring
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- operators/resource-broker/cmd/main.go
+++ operators/resource-broker/cmd/main.go
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// The main package running the resource-broker.
+// This package is running the resource-broker.
 package main
 
 import (
